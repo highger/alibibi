202097a8200327bbab296daed1b8e36b2c070b04
maguangyao <maguangyao@souche.com>
2019-08-28 23:56:19 +0800

    初始化项目

---
+++ src/main/java/com/mgy/SimplecrudApplication.java
@@ -0,0 +1,12 @@
+package com.mgy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplecrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplecrudApplication.class, args);
+	}
+}
--- src/test/java/com/mgy/SimplecrudApplicationTests.java
+++ src/test/java/com/mgy/SimplecrudApplicationTests.java
@@ -1,4 +1,4 @@
-package com.mgy.project;
+package com.mgy;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,10 +7,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ProjectApplicationTests {
+public class SimplecrudApplicationTests {
 
-    @Test
-    public void contextLoads() {
-    }
+	@Test
+	public void contextLoads() {
+	}
 
 }

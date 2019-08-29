5cb72dad4d86f5a5273985e19a6e549cfa6ea5c1
maguangyao <maguangyao@souche.com>
2019-08-29 11:44:17 +0800

    项目打包发布

--- src/main/java/com/mgy/SimplecrudApplication.java
+++ src/main/java/com/mgy/SimplecrudApplication.java
@@ -2,12 +2,20 @@ package com.mgy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SimplecrudApplication {
+public class SimplecrudApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
         System.out.println("项目启动后访问路径：http://localhost:8080/customers");
-	    SpringApplication.run(SimplecrudApplication.class, args);
-	}
+        SpringApplication.run(SimplecrudApplication.class, args);
+    }
+
+    @Override//为了打包springboot项目
+    protected SpringApplicationBuilder configure(
+            SpringApplicationBuilder builder) {
+        return builder.sources(this.getClass());
+    }
 }
